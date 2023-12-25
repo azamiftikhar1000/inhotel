9b008b195951b46aac173829e91e56e141e25738
Azam Iftikhar <azamiftikhar1000@gmail.com>
2023-12-25 18:30:21 +0530

    Edits

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 app.use(express.static('public'));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'customise.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, () => {
