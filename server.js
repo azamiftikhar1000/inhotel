5865b3ae8f3d69e9fb6c7d5d2a497546d86c1b2b
Azam Iftikhar <azamiftikhar1000@gmail.com>
2023-12-25 18:28:40 +0530

    url changes

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 app.use(express.static('public'));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'customise.html'));
 });
 
 app.listen(port, () => {
